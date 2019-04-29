9788ea4fb77084f70196cf3337475bd201a06daa
Jeremy Matt <jeremy.matt@gmail.com>
2019-04-29 10:47:14 -0400

    before finishing refactoring driver.py

--- driver.py
+++ driver.py
@@ -10,46 +10,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import re
-import urllib
+from causalinference import CausalModel as CM
 
 #JEM functions
 from PerPersVar_condense import PerPersVar_condense
 from drop_illogical import drop_illogical
 from make_hist_fig import make_hist_fig
+from load_data import load_data
 
 #Local locn of datafile
 data_fn = 'data\\Combined_n170c.csv'
 #Local location of variables descriptor file
 variables_fn = 'data\\variables.csv'
-
+#Set of variables to include
+VariableSet = 'toy'
 #Remote location of data file
 URL = 'https://www.dropbox.com/s/mdyfysm8t583v9p/Combined_n170c.csv?dl=1'
+#Drop records with missing response variable
+#Drop columns not in the selected set of variables
+Drop_extra=True
 
-#If the data file exists locally, load it using pandas
-try: df = pd.read_csv(data_fn)
-except: 
-    #If the data file does not exist locally, download and save locally, then open
-    print('Downloading data file (~430mb)')
-    urllib.request.urlretrieve(URL,data_fn)
-    print('Download Complete')
-    df = pd.read_csv(data_fn)
+#Load the data and begin processing
+DataSubset,variables,responseVar=load_data(data_fn,variables_fn,URL,VariableSet,Drop_extra)
+#
+##If the data file exists locally, load it using pandas
+#try: df = pd.read_csv(data_fn)
+#except: 
+#    #If the data file does not exist locally, download and save locally, then open
+#    print('Downloading data file (~430mb)')
+#    urllib.request.urlretrieve(URL,data_fn)
+#    print('Download Complete')
+#    df = pd.read_csv(data_fn)
+#
+##Read the variables descriptor file
+#variables = pd.read_csv(variables_fn)
+#
+#responseVar = variables['Variable'][variables['Response']=='y'].values[0]
+#
+##remove entries with negative response variables (non-responding person, N/A, not asked, etc)
+#VariableSet = 'toy'
+#NumRecords = df.shape[0]
+#DataSubset = pd.DataFrame(df[df[responseVar]>-1])
+#print('\n{} records dropped due to lack of response variable (non-responding person, N/A, etc)'.format(NumRecords-DataSubset.shape[0]))
+#
+#
+#KeepVars = variables['Variable'][variables[VariableSet]=='y']
+#
+#DataSubset = DataSubset[KeepVars]
 
-#Read the variables descriptor file
-variables = pd.read_csv(variables_fn)
 
 
-responseVar = variables['Variable'][variables['Response']=='y'].values[0]
+#Variables of 'discretionary' consumption (cigarettes, alcohol, meals out, etc)
+DiscVars = variables['Variable'][variables['Disc']=='y']
+#Build a 'discretionary' consumption variable add to the dataset
+DataSubset['DiscCon'] = DataSubset[DiscVars].sum(axis=1)
 
-#remove entries with negative response variables (non-responding person, N/A, not asked, etc)
-VariableSet = 'toy'
-NumRecords = df.shape[0]
-DataSubset = pd.DataFrame(df[df[responseVar]>-1])
-print('\n{} records dropped due to lack of response variable (non-responding person, N/A, etc)'.format(NumRecords-DataSubset.shape[0]))
+#List of clothing spending variables
+ClothVars = variables['Variable'][variables['Clothing']=='y']
+#Build clothign spending variable and add to the dataset
+DataSubset['ClothCon'] = DataSubset[ClothVars].sum(axis=1)
 
 
-KeepVars = variables['Variable'][variables[VariableSet]=='y']
-
-DataSubset = DataSubset[KeepVars]
+#Find flags (values less than 0) and set all flags to -1.
+DataSubset[DataSubset<0] = -1
 
 
 plt.hist(DataSubset[responseVar],bins=11)
@@ -64,30 +87,21 @@ plt.hist(DataSubset[var_p]-DataSubset[var_n],log=True,bins=100)
 DataSubset[var_p].max()
 DataSubset[var_n].mode()
 
-#Variables of 'discretionary' consumption (cigarettes, alcohol, meals out, etc)
-DiscVars = variables['Variable'][variables['Disc']=='y']
-
 #Check for differences between imputed and non-imputed variables
-dv1 = list(DiscVars)
-dv2 = [val+'i' for val in dv1]
+dv1 = list(DiscVars) #non-imputed
+dv2 = [val+'i' for val in dv1]  #imputed
 print('\nCheck for differences between imputed and non-imputed variables')
 for i in range(3):
     test = DataSubset[dv1[i]]-DataSubset[dv1[i]]
     print('{} minus {}==> min:{} max:{}'.format(dv1[i],dv2[i],test.min(),test.max()))
 
-#Build a 'discretionary' consumption variable add to the dataset
-DataSubset['DiscCon'] = DataSubset[DiscVars].sum(axis=1)
 #Drop the source variables
 DataSubset.drop(DiscVars,axis='columns',inplace=True)
 
-#List of clothing spending variables
-ClothVars = variables['Variable'][variables['Clothing']=='y']
-#Build clothign spending variable and add to the dataset
-DataSubset['ClothCon'] = DataSubset[ClothVars].sum(axis=1)
 #Drop the source variables
 DataSubset.drop(ClothVars,axis='columns',inplace=True)
 
-
+    
 
 
 total_p = 'nhifeftp' #total income
@@ -157,9 +171,13 @@ LocName = list(LocVals.keys())
 LocNames = dict(zip(LocList,LocName))
 
 
-CurData = drop_illogical(CurData,'disposable_income','total_income')
-CurData = drop_illogical(CurData,'ClothCon','total_income')
-CurData = drop_illogical(CurData,'DiscCon','total_income')
+DataSubset = drop_illogical(DataSubset,'disposable_income','total_income')
+DataSubset = drop_illogical(DataSubset,'ClothCon','total_income')
+DataSubset = drop_illogical(DataSubset,'DiscCon','total_income')
+
+DataSubset['DiscCon'][DataSubset['DiscCon']<0] = np.nan     #negative spending on alc/cig/meal
+DataSubset['ClothCon'][DataSubset['ClothCon']<0] = np.nan   #neg spend on clothing
+DataSubset['njbhruw'][DataSubset['njbhruw']>7*24] = np.nan  # 
 
 AreaData = pd.DataFrame()
 NumSubjects = 0
@@ -168,15 +186,17 @@ for loc in LocList:
     CurNum = CurData.shape[0]
     NumSubjects += CurNum
     print('{} participants in {}'.format(CurNum,LocNames[loc]))
-    mean = CurData['total_income'].mean()
-    std =  CurData['total_income'].std()
-    clothing_mean = CurData['ClothCon'].mean()
-    disc_mean = CurData['DiscCon'].mean()
+    CurData['income_mean']=CurData['total_income'].mean()
+    CurData['clothing_mean'] = CurData['ClothCon'].mean()
+    CurData['disc_mean'] = CurData['DiscCon'].mean()
     
-    CurData['income_ratio']=CurData['total_income']/mean
-    CurData['income_std'] = std
-    CurData['clothing_mean'] = CurData['ClothCon']/clothing_mean
-    CurData['disc_mean'] = CurData['DiscCon']/disc_mean
+    CurData['income_std'] = CurData['total_income'].std()
+    CurData['clothing_std'] = CurData['ClothCon'].std()
+    CurData['disc_std'] = CurData['DiscCon'].std()
+    
+    CurData['income_median'] = CurData['total_income'].median()
+    CurData['clothing_mmedian'] = CurData['ClothCon'].median()
+    CurData['disc_median'] = CurData['DiscCon'].median()
     AreaData = pd.concat([AreaData,CurData])
 print('{} subjects in total ({} non-capital-city subjects dropped)'.format(NumSubjects,DataSubset.shape[0]-NumSubjects)) 
 
@@ -185,20 +205,64 @@ sns.pairplot(AreaData[['total_income','disposable_income','DiscCon','ClothCon',r
 
 
 
-#Extract the per-person variables
+#Extract the per-person variables in the current data set
 PerPersVars = variables['Variable'][(variables[VariableSet]=='y')&(variables['per_person']=='y')]
 #Find the roots of the per-person variables
 VarRoots = set([val[:re.search('\d',val).start(0)] for val in PerPersVars])
 per_num_var = 'nhhpno'
 for root in VarRoots:
     AreaData = PerPersVar_condense(AreaData,root,per_num_var,DropSource=True)
-    
-save_dir = 'Figures\\simple_hist\\'
-var_descriptions = dict(zip(list(variables['Variable']),list(variables['Label'])))
-for var_name in AreaData.keys()[4:]:
-    data = list(AreaData[var_name])
-    num_bins = np.sqrt(len(data)).round().astype(int)
-    make_hist_fig(data,num_bins,save_dir,var_name,var_descriptions)
+
+ 
+makeplots = False
+if makeplots:
+    save_dir = 'Figures\\simple_hist\\'
+    var_descriptions = dict(zip(list(variables['Variable']),list(variables['Label'])))
+    for var_name in AreaData.keys()[4:]:
+        data = list(AreaData[var_name])
+        num_bins = np.sqrt(len(data)).round().astype(int)
+        make_hist_fig(data,num_bins,save_dir,var_name,var_descriptions)
+        
+plt.hist(AreaData['nhwnetwn'][AreaData['nhwnetwn']<2000000],bins=10,log=True)
+
+
+#Variables that have a very high proportion of missingness and/or are duplicated
+#by another variable that appears to have less missingness
+NoInfoToDrop = [
+        'ncnsc_gu',
+        'ncnsc_ge',
+        'ncnsc_au',
+        'ncnsc_ae',
+        'ncnsc_fo',
+        'ncnsc_ft',
+        'ncnsc_ps',
+        'ncnsc_fd',
+        'ncnsc_pd',
+        'ncnsc_fc',
+        'nmdhagt',
+        'nmdhamt',
+        'nmdhafu',
+        'nmdhash',
+        'nmdhapm',
+        'nmdhawc',
+        'nmdhatv',
+        'nmdhasm',
+        'nmdhawh',
+        'nmdharg',
+        'nmdhaph',
+        'nmdhahci',
+        'nmdhawm',
+        'nmdhaai',
+        'nmdhamv',
+        'nmdhamvi',
+        'nmdhasa',
+        'nmdhasl',
+        'nmdhadt',
+        'nmdhabp',
+        'nmdhahaw']
+
+Questionable_todrop = [
+        'nchctc']
 
 #
 #SydneyData = DataSubset[DataSubset.keys()][DataSubset[LocVar]==LocVals['Sydney']]
---
+++ load_data.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 22:15:24 2019
+
+@author: jmatt
+"""
+import pandas as pd
+import urllib
+
+
+def load_data(data_fn,variables_fn,URL,VariableSet,Drop_extra=True):
+    """
+    Takes the filename of the data file (data_fn) and the path/filename of the
+    variables description file, loads them, and returns the associated pandas
+    dataframes
+    
+    INPUTS
+        data_fn - path/filename of the data file
+        variables_fn - path/filename of a file describing the variables
+        URL - http address of download link for the data file (if not available
+              locally)
+        Drop_extra - (default: True) drop:
+                                        rows where the response variable is missing
+                                        columns not in the selected variable list
+        
+    OUTPUTS
+        DataSubset - pandas dataframe containing the data
+        variables - pandas dataaframe containing variable metadata
+        responseVar - the response variable column name
+    """
+    
+    #If the data file exists locally, load it using pandas
+    try: df = pd.read_csv(data_fn)
+    except: 
+        #If the data file does not exist locally, download and save locally, then open
+        print('Downloading data file (~430mb)')
+        urllib.request.urlretrieve(URL,data_fn)
+        print('Download Complete')
+        df = pd.read_csv(data_fn)
+        
+    print('\n************\nNOTE RE: MIXED TYPE WARNING - These variables contain:')
+    print('    294 - HQ ID of person providing most of the information for this form')
+    print('    295 - HQ ID of 1st Other providing information for this form')
+    print('    701 - HF2 Cross wave ID (xwaveid) - 0001')
+    print('    702 - HF2 Cross wave ID (xwaveid) - 0002')
+    print('These variables are not used in this project so the mixed type warning can be ignored\n************')
+    
+    #Read the variables descriptor file
+    variables = pd.read_csv(variables_fn)
+    
+    #extract the name of the response variable
+    responseVar = variables['Variable'][variables['Response']=='y'].values[0]
+    
+    if Drop_extra:
+        #Store the current number of recores
+        NumRecords = df.shape[0]
+        #remove entries with negative response variables (non-responding person, N/A, not asked, etc)
+        DataSubset = pd.DataFrame(df[df[responseVar]>-1])
+        #Notify the user how of how many records were dropped
+        print('\n{} records dropped due to lack of response variable (non-responding person, N/A, etc)'.format(NumRecords-DataSubset.shape[0]))
+        
+        #Extract the variable names for the variable set being analyzed
+        KeepVars = variables['Variable'][variables[VariableSet]=='y']
+        
+        #Keep only the variables in the variable set
+        DataSubset = DataSubset[KeepVars]
+        
+    else:
+        #Return all variables and records
+        DataSubset = df
+        
+    return DataSubset,variables,responseVar
+        
