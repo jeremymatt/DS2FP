510aecf9871db5b2218798322922413362460a67
Jeremy Matt <jeremy.matt@gmail.com>
2019-05-04 21:55:35 -0400

    improvements to plotting & remove combined mat_dep var

--- driver.py
+++ driver.py
@@ -16,14 +16,14 @@ import sys
 import os
 from causalinference import CausalModel
 
-try: 
-    import dowhy
-except:
-    str1 = '\n\n==> DoWhy causal inference package did not load, may not be installed\n'
-    str2 = '==> Installation Instructions:\n'
-    str3 = '==>    1. Clone the repository https://github.com/Microsoft/dowhy.git\n'
-    str4 = '==>    2. Run python setup.py install from the repo directory'
-    sys.exit(str1+str2+str3+str4)
+#try: 
+#    import dowhy
+#except:
+#    str1 = '\n\n==> DoWhy causal inference package did not load, may not be installed\n'
+#    str2 = '==> Installation Instructions:\n'
+#    str3 = '==>    1. Clone the repository https://github.com/Microsoft/dowhy.git\n'
+#    str4 = '==>    2. Run python setup.py install from the repo directory'
+#    sys.exit(str1+str2+str3+str4)
 #
 #from dowhy.do_why import CausalModel
 #import dowhy.plotter
@@ -136,6 +136,7 @@ vars_exclude_from_x = ['xwaveid','nhhrhid','nhhpno','nhhrpid']
 vars_exclude_from_x.extend(predictor_centrals)
 vars_exclude_from_x.extend(['treatment_binary','nlosat','treatment'])
 vars_exclude_from_x.extend(['total_income','total_disc','total_clothing'])
+vars_exclude_from_x.extend(['disposable_income','mat_dep'])
 
 AreaData_small = pd.DataFrame(AreaData)
 AreaData_small.drop(to_drop,axis='columns',inplace=True)
@@ -146,10 +147,45 @@ AreaData_small.drop(to_drop,axis='columns',inplace=True)
 
 #AreaData_test['treatment'] = AreaData_test['total_income']/AreaData_test['income_median']
 source_var_list = ['income','disc','clothing']
+var_to_titles = {
+        'income':'total income',
+        'disc':'discretionary spending',
+        'clothing':'clothing spending'}
 central_tendancy = 'mean'
 #central_tendancy = 'median'
-effect =[]
-for source_var in source_var_list:
+
+
+# make figures better for projector:
+font = {'weight':'normal','size':14}
+matplotlib.rc('font', **font)
+matplotlib.rc('figure', figsize=(7.0, 5.0))
+matplotlib.rc('xtick', labelsize=18) 
+matplotlib.rc('ytick', labelsize=18) 
+matplotlib.rc('legend',**{'fontsize':18})
+
+f, axarr = plt.subplots(3, sharex=True, figsize=(13, 11), dpi= 80, facecolor='w', edgecolor='k')
+
+for ax in axarr:
+    ax.set_ylabel('Norm. diff. \nin avg. covariate',fontsize=18)
+    
+cutoff = [.3,.3,.3]
+tuples = list(zip(source_var_list,axarr,cutoff))
+pre_effect = []
+effect_ols = []
+effect_match = []
+
+
+#AreaData_small.drop(admin_variables,axis='columns',inplace=True)
+x_vars = [val for val in AreaData_small.keys() if val not in vars_exclude_from_x]
+
+var_to_skip = 'mat_dep3'
+x_vars = [val for val in x_vars if val!=var_to_skip]
+
+x_var_descriptions = {}
+for var in x_vars:
+    x_var_descriptions[var] = list(variables[variables['Variable']==var]['Label'])
+
+for source_var,ax,cut in tuples:
     AreaData_small['treatment'] = AreaData_small['total_'+source_var]/AreaData_small[source_var+'_'+central_tendancy]
     AreaData_small['treatment_binary'] = 0
     AreaData_small.loc[AreaData_small['treatment']>1,'treatment_binary'] = 1
@@ -163,11 +199,6 @@ for source_var in source_var_list:
     
     D = np.array(AreaData_small['treatment_binary'])
     Y = np.array(AreaData_small['nlosat'])
-    #AreaData_small.drop(admin_variables,axis='columns',inplace=True)
-    x_vars = [val for val in AreaData_small.keys() if val not in vars_exclude_from_x]
-    
-    var_to_skip = 'mat_dep3'
-    x_vars = [val for val in x_vars if val!=var_to_skip]
     
     X = np.array(AreaData_small[x_vars])
     
@@ -180,10 +211,17 @@ for source_var in source_var_list:
     n_diff_init = causal.summary_stats['ndiff']
     
     causal.est_via_ols()
+    ate = causal.estimates['ols']['ate']
+    ate_se = causal.estimates['ols']['ate_se']
+    
+    pre_effect.append((ate,ate_se))
+    
+    
+    
     #print(causal.estimates)
     causal.est_propensity()
     #print(causal.propensity)
-    causal.cutoff
+    causal.cutoff=cut
     causal.trim()
     #causal.cutoff
     #print(causal.summary_stats)
@@ -199,40 +237,66 @@ for source_var in source_var_list:
     causal.est_via_weighting()
     #causal.est_via_blocking()
     causal.est_via_matching(bias_adj=True)
-    print(causal.estimates)
-    print(causal.summary_stats)
+#    print(causal.estimates)
+#    print(causal.summary_stats)
     
     
     r_diff_final = causal.summary_stats['rdiff']
     n_diff_final = causal.summary_stats['ndiff']
+    ate = causal.estimates['ols']['ate']
+    ate_se = causal.estimates['ols']['ate_se']
+    
+    effect_ols.append((ate,ate_se))
+    
     ate = causal.estimates['matching']['ate']
     ate_se = causal.estimates['matching']['ate_se']
-    
-    effect.append((ate,ate_se))
+    effect_match.append((ate,ate_se))
     
     #plt.bar(x_vars,n_diff_init)
     #plt.bar(x_vars,n_diff_final)
     
-    # make figures better for projector:
-    font = {'weight':'normal','size':14}
-    matplotlib.rc('font', **font)
-    matplotlib.rc('figure', figsize=(7.0, 5.0))
-    matplotlib.rc('xtick', labelsize=18) 
-    matplotlib.rc('ytick', labelsize=18) 
-    matplotlib.rc('legend',**{'fontsize':18})
     
-    plt.figure()
+#    plt.figure()
     ddff = pd.DataFrame(np.c_[n_diff_init,n_diff_final],index=x_vars)
     ddff.rename({0:'before_match',1:'after_match'},axis='columns',inplace=True)
-    ddff.plot.bar()
-    plt.savefig(source_var+'-'+central_tendancy+'-balance.png')
-    plt.close()
+    ddff.plot.bar(ax=ax)
+    ax.set_title(var_to_titles[source_var])
 
 
-y,std = list(zip(*effect))
-CE = 1.96*np.array(std)
-plt.errorbar(['income','discretionary','clothing'],y,yerr =CE,fmt='*',capsize = 10)
-plt.plot(['total income','discretionary','clothing'],[0,0,0])
+f.savefig('Combined-'+central_tendancy+'-balance.png')
+#    plt.close()
+    
+
+
+xlabels = ['income','discretionary','clothing']
+
+matplotlib.rc('figure', figsize=(9.0, 6.0))
+
+
+pre_y,pre_std = list(zip(*pre_effect))
+pre_CE = 1.96*np.array(pre_std)
+
+y_ols,std_ols = list(zip(*effect_ols))
+CE_ols = 1.96*np.array(std_ols)
+
+y_match,std_match = list(zip(*effect_match))
+CE_match = 1.96*np.array(std_match)
+
+offset = .15
+pre_ols_xlocs = np.array([1,2,3])
+post_ols_xlocs = pre_ols_xlocs+offset
+post_match_xlocs = pre_ols_xlocs+offset*2
+
+plt.figure()
+plt.errorbar(pre_ols_xlocs,pre_y,yerr =pre_CE,fmt='m*',capsize = 10, markersize=12,label='pre-match (OLS)')
+plt.errorbar(post_ols_xlocs,y_ols,yerr =CE_ols,fmt='bo',capsize = 10, markersize=12,label='post-match (OLS)')
+plt.errorbar(post_match_xlocs,y_match,yerr =CE_match,fmt='ro',capsize = 10, markersize=12,label='post-match (matching)')
+plt.xticks(post_ols_xlocs,xlabels)
+plt.legend()
+plt.plot([1,3.5],[0,0],'b:')
+plt.ylabel('Effect Size (change in SWB)')
+plt.savefig('effect_summary.png')
+#plt.show()
 
 #
 ##AreaData_small.drop('total_income',axis='columns',inplace=True)
